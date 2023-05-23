2e55b66b8a02c05414455ec88fcb73ea2624911d
KanzaTahreem <tehreemmsk411@gmail.com>
2023-05-24 00:33:40 +0500

    Add clients data

---
+++ src/components/ClientsData.jsx
@@ -0,0 +1,173 @@
+const ClientsData = [
+  {
+    serial: 1,
+    previousType: '소득적격',
+    applicationType: '개인전문',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인대기',
+    reason: '',
+    approvalDate: '',
+    admin: ''
+  },
+  {
+    serial: 2,
+    previousType: '소득적격',
+    applicationType: '개인전문',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인거부',
+    reason: '서류 식별 불가 금융투자업자에 계좌를 개설한지 1년 미만으로 전문투자자 승인 불가',
+    approvalDate: '2023-01-10 09:00:00',
+    admin: '김관리자'
+  },
+  {
+    serial: 3,
+    previousType: '소득적격',
+    applicationType: '소득적격',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인완료',
+    reason: '',
+    approvalDate: '2023-01-10 09:00:00',
+    admin: '김관리자'
+  },
+
+  {
+    serial: 4,
+    previousType: '소득적격',
+    applicationType: '개인전문',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인대기',
+    reason: '',
+    approvalDate: '',
+    admin: ''
+  },
+  {
+    serial: 5,
+    previousType: '소득적격',
+    applicationType: '개인전문',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인거부',
+    reason: '서류 식별 불가 금융투자업자에 계좌를 개설한지 1년 미만으로 전문투자자 승인 불가',
+    approvalDate: '2023-01-10 09:00:00',
+    admin: '김관리자'
+  },
+  {
+    serial: 6,
+    previousType: '소득적격',
+    applicationType: '소득적격',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인완료',
+    reason: '',
+    approvalDate: '2023-01-10 09:00:00',
+    admin: '김관리자'
+  },
+
+  {
+    serial: 7,
+    previousType: '소득적격',
+    applicationType: '개인전문',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인대기',
+    reason: '',
+    approvalDate: '',
+    admin: ''
+  },
+  {
+    serial: 8,
+    previousType: '소득적격',
+    applicationType: '개인전문',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인거부',
+    reason: '서류 식별 불가 금융투자업자에 계좌를 개설한지 1년 미만으로 전문투자자 승인 불가',
+    approvalDate: '2023-01-10 09:00:00',
+    admin: '김관리자'
+  },
+  {
+    serial: 9,
+    previousType: '소득적격',
+    applicationType: '소득적격',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인완료',
+    reason: '',
+    approvalDate: '2023-01-10 09:00:00',
+    admin: '김관리자'
+  },
+
+  {
+    serial: 10,
+    previousType: '소득적격',
+    applicationType: '개인전문',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인대기',
+    reason: '',
+    approvalDate: '',
+    admin: ''
+  },
+  {
+    serial: 11,
+    previousType: '소득적격',
+    applicationType: '개인전문',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인거부',
+    reason: '서류 식별 불가 금융투자업자에 계좌를 개설한지 1년 미만으로 전문투자자 승인 불가',
+    approvalDate: '2023-01-10 09:00:00',
+    admin: '김관리자'
+  },
+  {
+    serial: 12,
+    previousType: '소득적격',
+    applicationType: '소득적격',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인완료',
+    reason: '',
+    approvalDate: '2023-01-10 09:00:00',
+    admin: '김관리자'
+  },
+
+  {
+    serial: 13,
+    previousType: '소득적격',
+    applicationType: '개인전문',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인대기',
+    reason: '',
+    approvalDate: '',
+    admin: ''
+  },
+  {
+    serial: 14,
+    previousType: '소득적격',
+    applicationType: '개인전문',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인거부',
+    reason: '서류 식별 불가 금융투자업자에 계좌를 개설한지 1년 미만으로 전문투자자 승인 불가',
+    approvalDate: '2023-01-10 09:00:00',
+    admin: '김관리자'
+  },
+  {
+    serial: 15,
+    previousType: '소득적격',
+    applicationType: '소득적격',
+    docs: '보기',
+    applicationDate: '2023-01-10 09:00:00',
+    approvalStatus: '승인완료',
+    reason: '',
+    approvalDate: '2023-01-10 09:00:00',
+    admin: '김관리자'
+  },
+];
+
+export default ClientsData;
