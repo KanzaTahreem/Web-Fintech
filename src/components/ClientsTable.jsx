8a3b8fbb0d2556b0953477393d8713356d71e0c5
KanzaTahreem <tehreemmsk411@gmail.com>
2023-05-24 00:37:28 +0500

    Generate clients table with relevant components

---
+++ src/components/ClientsTable.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import ClientsData from './ClientsData';
+import TableHead from './TableHead'
+import TableRow from './TableRow'
+import leftDoubleArrow from '../assets/images/double_left_arrow.svg'
+import rightDoubleArrow from '../assets/images/double_right_arrow.svg'
+import leftArrow from '../assets/images/left_arrow.svg'
+import rightArrow from '../assets/images/right_arrow.svg'
+import styles from '../styles/dashboard.module.css';
+
+
+const ClientsTable = () => {
+  return (
+    <>
+      <div className={styles.clients_table}>
+        <table>
+          <TableHead />
+          <tbody>
+            {ClientsData.map((item, index) => (
+                <TableRow key={index} item={item} />
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.pagination_frame}>
+        <div className={styles.pagination}>
+          <div className={styles.arrows}>
+            <img src={leftDoubleArrow} alt="left_arrow" />
+            <img src={leftArrow} alt="left_arrow" />
+          </div>
+          <div className={styles.numbers}>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>5</p>
+            <p>6</p>
+            <p>7</p>
+            <p>8</p>
+            <p>9</p>
+            <p>10</p>
+          </div>
+          <div className={styles.arrows}>
+            <img src={rightArrow} alt="right_arrow" />
+            <img src={rightDoubleArrow} alt="right_arrow" />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ClientsTable;
