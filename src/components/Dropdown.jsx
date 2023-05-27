d5d63f5e8b0932f23816aa41dc8690374f6e77f7
KanzaTahreem <tehreemmsk411@gmail.com>
2023-05-27 22:56:07 +0500

    Disable dropdown on unchecked checkboxes

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -3,11 +3,14 @@ import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 import styles from '../styles/dashboard.module.css';
 import modalStyles from '../styles/modal.module.css';
 
-const Dropdown = ({ buttonText, menuItems, selectedItem, setSelectedItem }) => {
+const Dropdown = ({ buttonText, menuItems, selectedItem, setSelectedItem, enabled }) => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
 
   const toggleDropdown = () => {
+    if (enabled !== undefined && enabled === 0) {
+      return;
+    }
     setIsOpen((prevState) => !prevState);
   };
 
@@ -31,9 +34,9 @@ const Dropdown = ({ buttonText, menuItems, selectedItem, setSelectedItem }) => {
 
   useEffect(() => {
     if (menuItems && menuItems.length > 0 && !selectedItem) {
-      setSelectedItem(menuItems[0]);
+      setSelectedItem(buttonText);
     }
-  }, [menuItems]);
+  }, []);
 
   return (
     <div ref={dropdownRef} className={`${styles.box} ${modalStyles.investment_dropdown} ${isOpen ? styles['is-open'] : ''}`}>
